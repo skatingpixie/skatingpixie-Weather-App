0267920a9e7db50a5138ca4a60818db953fbb949
Charlotte Millward <114440578+skatingpixie@users.noreply.github.com>
2023-01-07 20:45:27 +0000

    Linked API to forecast & formatted data requests
    Forecast API linked for current location and search location updated. Formatted the data responses in the HTML loop.

--- src/app.js
+++ src/app.js
@@ -46,6 +46,25 @@ let imgSrc = [
   "/src/images/weather-app/weather-hero-img/clouds.png",
 ];
 
+//formatting day
+
+function formatDay(dt) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let date = new Date(dt * 1000);
+  let day = days[date.getDay()];
+
+  return day;
+}
+
 //current temperature, description, city & weather icon
 function temp(response) {
   function arrayMatch(value) {
@@ -76,6 +95,7 @@ function temp(response) {
   geoCity.innerHTML = `${city}, ${country}`;
 
   desc.innerHTML = `It is currently ${weatherDescriptionArray[index]} in ${city}, ${country}.`;
+  getForecast(response.data.coord);
 }
 
 function showPosition(position) {
@@ -89,28 +109,35 @@ function showPosition(position) {
 navigator.geolocation.getCurrentPosition(showPosition);
 
 //5 day weather forecast
-function weatherForecast() {
-  console.log(response.data);
+function weatherForecast(response) {
+  let forecastData = response.data.daily;
   let forecast = document.querySelector("#weather-forecast");
   let forecastHTML = `<div class="row">`;
 
-  let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecastData.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
   <div class="col-md-12 border border-2 rounded-3">
     <div class="day-wrapper">
       <ul>
-        <li class="weather-icon-sm forecast-icon">🌞</li>
+        <li class="weather-icon-sm forecast-icon"><img src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png" alt="daily-weather-icon" height=46px/></li>
         <div class="data-align">
-          <li><span class="forecast-day">${day}</span></li>
-          <li><strong id="forecast-temp">8°C</strong></li>
+          <li><span class="forecast-day">${formatDay(
+            forecastDay.dt
+          )}</span></li>
+          <li><strong id="forecast-temp">${Math.round(
+            forecastDay.temp.day
+          )}°C</strong></li>
         </div>
       </ul>
     </div>
   </div>
 `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
@@ -120,7 +147,6 @@ function weatherForecast() {
 function getForecast(coordinates) {
   let apiKey = "7d048e6e84d3235680d2c650732b34fb";
   let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(weatherForecast);
 }
 
